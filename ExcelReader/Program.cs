aa5a0c8e25c88b291109a2aa8c5bb52d91989002
xXAinzXx <dominic.tosku@gmail.com>
2024-06-01 19:37:23 +0200

    feat: use radzen components

--- ExcelReader/Program.cs
+++ ExcelReader/Program.cs
@@ -4,6 +4,7 @@ using ExcelReader.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using OfficeOpenXml;
+using Radzen;
 ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -11,6 +12,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddRadzenComponents();
+builder.Services.AddScoped<ContextMenuService>();
+builder.Services.AddScoped<DialogService>();
 builder.Services.AddTransient<IExcelService, ExcelService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
